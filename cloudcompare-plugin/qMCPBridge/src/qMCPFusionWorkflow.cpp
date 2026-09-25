79ad6335b866883239290ec2f48dab82d3c6db86
Grant Ross <grantross64@gmail.com>
2026-09-25 13:30:51 -0700

    Bump qMCPBridge workflow version to 0.4.1

--- cloudcompare-plugin/qMCPBridge/src/qMCPFusionWorkflow.cpp
+++ cloudcompare-plugin/qMCPBridge/src/qMCPFusionWorkflow.cpp
@@ -944,7 +944,7 @@ QJsonObject capabilities()
     result[ "global_coordinate_export" ] =
         "CloudCompare PLY and OBJ writers emit global coordinates using stored global shift/scale.";
 
-    result[ "plugin_version" ] = "0.4.0";
+    result[ "plugin_version" ] = "0.4.1";
 
     QJsonArray bridgeOperations{
         "ping",
